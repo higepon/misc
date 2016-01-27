390843ebe82b27571685591455b1d935aed4226b
Taro Minowa <higepon@gmail.com>
2016-01-26 21:18:31 -0800

    implement-strstr

---
+++ implement-strstr.rb
@@ -0,0 +1,32 @@
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+  if needle.empty? && needle.empty?
+    return 0
+  elsif needle.size > haystack.size
+    return -1
+  end
+  i = 0
+  while i < haystack.size
+    chl = haystack[i]
+    j = 0
+    allEqual = true
+    k = i
+    while k < haystack.size && j < needle.size
+      if haystack[k] != needle[j]
+        allEqual = false
+        break
+      end
+      k = k + 1
+      j = j + 1
+    end
+    if allEqual && j == needle.size
+      return i
+    else
+      # no op
+    end
+    i = i + 1
+  end
+  return -1
+end
